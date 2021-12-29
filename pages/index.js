879c8c40164e136cf7b7c700567e75d3c5ab0819
leooos33 <sigmaw39@gmail.com>
2021-12-29 11:52:49 +0200

    fav

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
+      <title>Wallet</title>
     </Head>
     <div class="mx-auto py-48 w-screen h-screen text-black bg-white flex flex-row justify-center text-center" >
           <h1 class="sm:text-3xl text-2xl xl:text-4xl 3xl:text-5xl title-font mb-4 font-sans font-bold">Successfully, now please return to the editor you are connected!</h1>
